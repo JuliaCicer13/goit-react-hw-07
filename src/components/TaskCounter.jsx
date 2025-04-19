93d24c0663c6c07c1662080958c4504e80cd4f84
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-04-20 02:15:25 +0300

    j

--- src/components/TaskCounter.jsx
+++ src/components/TaskCounter.jsx
@@ -1,23 +1,11 @@
 import { useSelector } from "react-redux";
-import { selectTasks } from "../redux/tasksSlice";
+import { selectTaskCount } from "../redux/tasksSlice";
 
 import css from "../components/styles/TaskCounter.module.css";
 
 export const TaskCounter = () => {
-    const tasks = useSelector((state) => state.tasks.items);
+    const count = useSelector(selectTaskCount);
 
-    const count = tasks.reduce(
-        (acc, task) => {
-            if (task.complited) {
-                acc.complited += 1;
-
-            } else {
-                acc.active += 1;
-            }
-            return acc;
-        },
-        { active: 0, complited: 0 }
-    );
 
     return (
         <div>
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -2,7 +2,21 @@ import { createSlice } from '@reduxjs/toolkit';
 // import operation
 import { fetchTasks, addTask, deleteTask, toggleCompleted } from "./operations";
 
+export const selectTaskCount = state => {
+  const tasks = selectTasks(state);
 
+  return tasks.reduce(
+    (count, task) => {
+      if (task.completed) {
+        count.completed += 1;
+      } else {
+        count.active += 1;
+      }
+      return count;
+    },
+    { active: 0, completed: 0 }
+  );
+};
 
 export const selectTasks = (state) => state.tasks.items;
 
